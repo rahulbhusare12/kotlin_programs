e61243616087c40968d25653dd726955c47ef4e8
yantra_rahul <rahul@yantramatic.com>
2025-07-29 16:04:40 +0530

    clean code

---
+++ src/main/kotlin/swapnesh/AlphanumericaString.kt
@@ -0,0 +1,12 @@
+
+fun main(){
+
+    var alphanumericString= "abc123xyz456"
+
+    val letter = alphanumericString.filter { it.isLetter() }
+
+    val num = alphanumericString.filter { it.isDigit() }
+
+    println(letter)
+    println(num)
+}
