60a2d33f2d0bbd76871e7bfd8094deeb129be3fa
Swapnesh <65058937+Swapnesh7677@users.noreply.github.com>
2025-07-29 11:07:21 +0530

    Add files via upload

---
+++ src/main/kotlin/ReversedStrinf.kt
@@ -0,0 +1,19 @@
+package com.swapnesh.myapplication.neetcode
+
+fun main(){
+
+    //raw
+    val str = "Kotlin Programming"
+
+    val words = str.split(" ")
+
+    val rvstr = words.joinToString(" ") { word ->
+        var newstr = ""
+        for(i in word.length-1 downTo  0){
+            newstr += word[i]
+        }
+        newstr
+    }
+
+    println(rvstr)
+}
